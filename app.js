e2cf36330bb523bbb034901364277009765a667e
Jessica Costa <jesscodes@proton.me>
2024-03-11 20:14:11 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 150; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(pokemon => {
+        console.log(pokemon);
+      });
+  }
