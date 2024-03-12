cb3c52b25ebdd24eee28d52e09e7c936b0ae0e40
Jessica Costa <jesscodes@proton.me>
2024-03-11 21:10:53 -0300

    JS completo

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+const pokedex = document.getElementById("pokedex");
+
+//
+
 const promises = [];
+
+//
 
 for (let i = 1; i <= 150; i++) {
   const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
@@ -6,12 +12,27 @@ for (let i = 1; i <= 150; i++) {
 }
 
 Promise.all(promises).then(results => {
-  console.log(results);
+  const pokemon = results.map(data => ({
+    name: data.name,
+    id: data.id,
+    image: data.sprites["front_default"],
+    type: data.types.map(type => type.type.name).join(", "),
+  }));
+  displayPokemon(pokemon);
 });
 
-const pokemon = results.map(data => ({
-  name: data.name,
-  id: data.id,
-  image: data.sprites["front_default"],
-  type: data.types.map(type => type.type.name).join(", "),
-}));
+const displayPokemon = pokemon => {
+  const pokemonHTMLString = pokemon
+  .map(
+    pokemon =>
+      `
+    <li class="card">
+        <img class="card-image" src="${pokemon.image}"/>
+        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+        <p class="card-subtitle">Type: ${pokemon.type}</p>
+    </li>
+    `
+  )
+  .join("");
+  pokedex.innerHTML = pokemonHTMLString;
+};
